69dbf54d6644014aafe0a19fd542f273e7630c69
Mark Lambert <Mark@mlambert.uk>
2023-04-11 08:21:25 +0100

    Update tests

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -1,8 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-using RomanNumerals;
+﻿using RomanNumerals;
+
+string passedValue = args.Length > 0 ? args[0].ToString() : "";
+
+if (passedValue == "")
+{
+    Console.WriteLine("Please enter an integer or Roman Numeral to convert:");
+
+    var response = Console.ReadLine();
+    if (response != null) passedValue = response;
+}
+
+if (passedValue =="")
+{
+    Console.WriteLine("Nothing to convert!");
+    return;
+}
 
 RomanNumeralConverter rnc = new RomanNumeralConverter();
 
-Console.WriteLine("Hello, World!");
-
-
+try
+{
+    int value = int.Parse(passedValue);
+    Console.WriteLine(rnc.ConvertToRomanNumerals(value));
+} catch {
+    Console.WriteLine(rnc.ConvertFromRomanNumerals(passedValue));
+}
